95fdcf8378734ce4dfd6b36bb7a56468bb77117d
Andy152101 <altamar.naye2104@gmail.com>
2025-07-22 17:15:42 -0500

    feat: crea usuario admin por defecto al iniciar

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
-import { createDefaultUser } from "./libs/createDefaultUser.js"; // 👈 Importa la función
+import { createDefaultUser } from "./libs/createDefaultUser.js"; // Importa la función
 
 async function main() {
   await connectDB(); // Conectamos a Mongo
-  await createDefaultUser(); // 👈 Creamos el admin si no existe
+  await createDefaultUser(); //  Creamos el admin si no existe
 
   app.listen(5000, () => {
     console.log("🚀 Server on port", 5000);
